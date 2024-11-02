c0c1f98969f9bfbd457c49e77f5873a827a2c8ae
AndreXVR <xavier.andre256@gmail.com>
2024-11-02 00:05:23 -0400

    Added token to register route, added AuthService

--- user-stash-app/src/app/register/register.component.ts
+++ user-stash-app/src/app/register/register.component.ts
@@ -1,5 +1,10 @@
+import { HttpClient } from "@angular/common/http";
 import { Component, inject } from "@angular/core";
 import { ReactiveFormsModule, FormBuilder, Validators } from "@angular/forms";
+import { environment } from "../../environments/environment";
+import { UserInterface } from "../../interfaces/user.interface";
+import { AuthService } from "../services/auth.service";
+import { Router } from "@angular/router";
 
 @Component({
     selector: "app-register",
@@ -9,6 +14,9 @@ import { ReactiveFormsModule, FormBuilder, Validators } from "@angular/forms";
 })
 export class RegisterComponent {
     fb = inject(FormBuilder);
+    http = inject(HttpClient);
+    authService = inject(AuthService);
+    router = inject(Router);
 
     form = this.fb.nonNullable.group({
         username: ["", Validators.required],
@@ -17,6 +25,15 @@ export class RegisterComponent {
     });
 
     onSubmit(): void {
-        console.log("register");
+        this.http.post<{user: UserInterface}>(
+            environment.apiUrl + "/users", {
+                user: this.form.getRawValue()
+            }
+        ).subscribe(response => {
+            console.log("response", response);
+            localStorage.setItem("token", response.user.token);
+            this.authService.currentUserSig.set(response.user);
+            this.router.navigateByUrl("/");
+        });
     }
 }
--- user-stash-server/user_stash/app.py
+++ user-stash-server/user_stash/app.py
@@ -1,8 +1,41 @@
-from flask import Flask
+from flask import Flask, request
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+CORS(app)
+users_list = []
 
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+@app.route("/users", methods=["POST"])
+def users():
+    if request.method == "POST":
+        user = request.json["user"]
+        response = {"user": {
+            "username": request.json["user"]["username"],
+            "email": request.json["user"]["email"],
+            "token": "abuble"
+        }}
+        users_list.append(user)  # Saving users on memory, for testing without DB
+    return response
+
+
+@app.route("/users/login", methods=["POST"])
+def login():
+    if request.method == "POST":
+        user = request.json["user"]
+        response = {"user": {
+            "username": user["username"],
+            "email": user["email"],
+            "token": "abuble"
+        }}
+    return response
+
+
+if __name__ == '__main__':
+    app.run(port=5000)
